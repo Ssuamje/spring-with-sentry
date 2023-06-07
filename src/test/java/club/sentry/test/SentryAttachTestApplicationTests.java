ab25f9084622d7de827309ed9b250d5b7df3abba
Ssuamje <side2850@naver.com>
2023-06-07 20:50:01 +0900

    프로젝트 생성

---
+++ src/test/java/club/sentry/test/SentryAttachTestApplicationTests.java
@@ -0,0 +1,13 @@
+package club.sentry.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentryAttachTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
